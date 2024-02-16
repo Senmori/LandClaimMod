4ca12466525746d41aeb5642e0c781109a3dee5b
Senmori <thesenmori@gmail.com>
2024-02-15 19:30:09 -0500

    Initial commit.

---
+++ LandClaimMod/LandClaimModModSystem.cs
@@ -0,0 +1,14 @@
+﻿using Vintagestory.API.Common;
+
+namespace LandClaimMod;
+
+public class LandClaimModModSystem : ModSystem
+{
+    private ICoreAPI api;
+    
+    public override void Start(ICoreAPI api)
+    {
+        base.Start(api);
+        this.api = api;
+    }
+}
